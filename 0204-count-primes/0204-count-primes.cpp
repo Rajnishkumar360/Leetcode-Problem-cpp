4198cc59103d8d7968de10bb1dfa1bddf5295e40
Rajnish kumar <rajnishkumar957241@gmail.com>
2026-08-20 12:38:09 +0530

    Time: 778 ms (5%), Space: 40.6 MB (18.95%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     int countPrimes(int n) {
-
         if (n <= 2)
             return 0;
-
-        // Only store odd numbers
         vector<bool> isPrime(n, true);
-
         isPrime[0] = isPrime[1] = false;
-
-        // All even numbers except 2 are not prime
         for (int i = 4; i < n; i += 2) {
             isPrime[i] = false;
         }
-
-        // Check only odd numbers
         for (int i = 3; i * i < n; i += 2) {
-
             if (isPrime[i]) {
-
                 for (int j = i * i; j < n; j += 2 * i) {
                     isPrime[j] = false;
                 }
             }
         }
 
-        int ans = 1; // count prime 2
-
+        int ans = 1;
         for (int i = 3; i < n; i += 2) {
             if (isPrime[i])
                 ans++;
