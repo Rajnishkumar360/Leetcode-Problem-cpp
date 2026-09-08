6fb907d9519b61550dd8897cf4dc991376b6116c
Rajnish kumar <rajnishkumar957241@gmail.com>
2026-09-08 10:51:25 +0530

    Time: 0 ms (100%), Space: 95.1 MB (60.92%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,36 +1,44 @@
-class Solution {
-public:
-    vector<int> mergeArray(vector<int>& arr1, vector<int>& arr2) {
-        int n = arr1.size();
-        int m = arr2.size();
-        int i = 0, j = 0;// two pointer
-        vector<int> ans;
-        while (i < n && j < m) {
-            if (arr1[i] <= arr2[j]) {
-                ans.push_back(arr1[i]);
-                i++;
+class Solution{
+    public:
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    if (nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1);
+    int n = nums1.size(), m = nums2.size();
+    int low = 0, high = n;
+    while (low <= high) {
+        int i = (low + high) / 2;
+        int j = (n + m + 1) / 2 - i;
+
+        int left1, right1, left2, right2;
+        if (i == 0) left1 = INT_MIN;
+
+        else left1 = nums1[i - 1];
+        if (i == n) right1 = INT_MAX;
+
+        else right1 = nums1[i];
+        if (j == 0) left2 = INT_MIN;
+
+        else left2 = nums2[j - 1];
+        if (j == m) right2 = INT_MAX;
+        
+        else right2 = nums2[j];
+        if (left1 <= right2 && left2 <= right1) {
+            if ((n + m) % 2 == 0) {
+                int maxLeft = (left1 > left2) ? left1 : left2;
+                int minRight = (right1 < right2) ? right1 : right2;
+                return (maxLeft + minRight) / 2.0;
             } else {
-                ans.push_back(arr2[j]);
-                j++;
+                if (left1 > left2) return left1;
+                else return left2;
             }
-        }
-        while (i < n) {
-            ans.push_back(arr1[i]);
-            i++;
-        }
-        while (j < m) {
-            ans.push_back(arr2[j]);
-            j++;
-        }
-        return ans;
-    }
-    double findMedianSortedArrays(vector<int>& arr1, vector<int>& arr2) {
-        vector<int> merged = mergeArray(arr1, arr2);
-        int t = merged.size();
-        if (t % 2 == 1) {
-            return merged[t / 2];
+        } else if (left1 > right2) {
+            high = i - 1;
         } else {
-            return (merged[t / 2 - 1] + merged[t / 2]) / 2.0;
+            low = i + 1;
         }
     }
+    return 0.0; 
+}
 };
+
+
